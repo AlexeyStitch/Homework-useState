3b24aa598da616ee32905fae00bb7c4119ebd799
Alexey Stitch <alexeyoffixial@yandex.ru>
2020-05-29 05:08:30 -0700

    UseState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import LeftHandler from "./LeftHandler";
+import RightHandler from "./RightHandler";
+import StepConfig from "./StepConfig";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [count, setCount] = useState(5);
+    const [step, setStep] = useState(2);
+
+    const handler = (value) => {
+        setCount(value);
+    }
+ const onStepChange = (value) => {
+        setStep(value);
+ }
+
+    return (
+        <div className="App">
+            <RightHandler count={count}/>
+            <LeftHandler count={count}
+                         changeCount={handler}
+                         step={step}/>
+                         <StepConfig step={step} onStepChange={onStepChange}/>
+
+        </div>
+    );
 }
 
 export default App;
